761f68cc93a921b603afa2cd56d79c03e550b179
Jing Zhang <jzhangc@gmail.com>
2021-02-05 14:55:28 -0500

    progress updates

--- pg_practice.session.sql
+++ pg_practice.session.sql
@@ -1,22 +1,38 @@
 ------ Create table  ------
 -- connect to the database using SQLTools in vscode before doing the following --
--- CREATE TABLE person (
---     id BIGSERIAL NOT NULL PRIMARY KEY,
---     first_name VARCHAR(50) NOT NULL,
---     last_name VARCHAR(50) NOT NULL,
---     gender VARCHAR(7) NOT NULL,
---     email VARCHAR(150) NOT NULL
--- );
--- add some recrod --
--- INSERT INTO person
--- VALUES (
---         1,
---         'John',
---         'Smith',
---         'male',
---         'john.smith@gmail.com'
---     ),
---     (2, 'Sarah', 'Jones', 'female', 'sarah.jones@company.com');
--- later data type --
+CREATE TABLE person (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    gender VARCHAR(7) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    email VARCHAR(150) NOT NULL
+);
 ALTER TABLE person
-ALTER COLUMN email DROP NOT NULL;
+    ALTER COLUMN email DROP NOT NULL;
+-- add some recrod --
+-- we add two, but one at a time
+INSERT INTO person (
+        first_name,
+        last_name,
+        gender,
+        date_of_birth
+    ) VALUES (
+        'Ann',
+        'Smith',
+        'FEMALE',
+        DATE '1988-01-09'
+    );
+INSERT INTO person (
+        first_name,
+        last_name,
+        gender, 
+        date_of_birth,
+        email
+    ) VALUES (
+        'Jake',
+        'Jones',
+        'MALE',
+        DATE '1990-12-31',
+        'jake@gmail.com'
+    );
