6f3503829b45e3ede5caf63ccbc828578ccfe0d0
Jing Zhang <jzhangc@gmail.com>
2021-02-19 16:00:00 -0500

    progress update

--- ch6_editing_data.sql
+++ ch6_editing_data.sql
@@ -1,3 +1,11 @@
 ------ update and edit tables ------
--- NULL handling --
+---- NULL handling ----
 SELECT COALESCE(null, 1);
+
+---- primary key ----
+-- show primary key: \d table (only in pgcli)--
+-- insert a record with exact same match in the person tabel (will not work) --
+SELECT * FROM person LIMIT 1;
+insert into person (id, first_name, last_name, gender, date_of_birth, country_of_birth, email) values (1, 'Tiffany', 'Fissenden', 'Genderqueer', '2002-04-30', 'United States', null);
+
+-- delete the primary key --
--- ch7_date_time type.sql
+++ ch7_date_time type.sql
@@ -5,3 +5,7 @@ SELECT NOW();
 -- elements --
 SELECT NOW()::DATE;
 SELECT NOW()::TIME;
+
+
+-- calculate age --
+SELECT first_name, last_name, gender, country_of_birth, date_of_birth, AGE(NOW(), date_of_birth) AS age FROM person;
