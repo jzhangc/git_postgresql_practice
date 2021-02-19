e655c1c466cf09c665893fd97b6c150f4211e18a
Jing Zhang <jzhangc@gmail.com>
2021-02-19 16:34:38 -0500

    progress update

--- ch6_editing_data.sql
+++ ch6_editing_data.sql
@@ -6,6 +6,11 @@ SELECT COALESCE(null, 1);
 -- show primary key: \d table (only in pgcli)--
 -- insert a record with exact same match in the person tabel (will not work) --
 SELECT * FROM person LIMIT 1;
-insert into person (id, first_name, last_name, gender, date_of_birth, country_of_birth, email) values (1, 'Tiffany', 'Fissenden', 'Genderqueer', '2002-04-30', 'United States', null);
+insert into person (first_name, last_name, gender, date_of_birth, country_of_birth, email) values ('Sarah', 'Fissenden', 'Genderqueer', '2002-04-30', 'United States', null);
 
 -- delete the primary key --
+
+-- adding a record to create duplicated email --
+insert into person (first_name, last_name, gender, date_of_birth, country_of_birth, email) values ('Sarah', 'Findlay', 'Genderfluid', '1994-04-22', 'Germany', 'nfindlay1@goo.ne.jp');
+
+---- unique constraint ------
